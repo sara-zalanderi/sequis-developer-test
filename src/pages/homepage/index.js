6c33822c97ce57d59a8b4cc3896d908bdbc434fc
sarazalanderi <sarazalanderi@gmail.com>
2023-08-16 19:41:07 +0700

    Add categories

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Grid, Image, Label } from "semantic-ui-react";
+import { Grid, Image, Label, Button, Menu } from "semantic-ui-react";
 import styled from "styled-components";
 
 const StyledCard = styled(Grid.Row)`
@@ -7,8 +7,33 @@ const StyledCard = styled(Grid.Row)`
     padding: 50px 100px;
     text-align: center;
   }
+  && .ui.basic.black.button {
+    border: none;
+    border-radius: 0;
+    box-shadow: none !important;
+    width: 100%;
+    text-align: left;
+    font-weight: bold;
+    padding-left: 0;
+  }
+  && .menu-border .ui.basic.black.button {
+    border-bottom: 2px solid black;
+  }
+  && .ui.basic.black.button.active {
+    color: #ff7518 !important;
+  }
   && img {
     object-fit: cover;
+    border-radius: 10px;
+  }
+  && .label {
+    padding: 5px;
+    text-transform: uppercase;
+    font-size: 0.7rem;
+    border-radius: 15px;
+  }
+  && .label span {
+    font-weight: 400;
   }
 `;
 
@@ -19,6 +44,7 @@ const Homepage = () => {
   const [articlesData, setArticlesData] = useState();
   const [featuredData, setFeaturedData] = useState();
   const [nonfeaturedData, setNonfeaturedData] = useState();
+  const [activeItem, setActiveItem] = useState("all");
   const [categoryData, setCategoryData] = useState({
     foodData: [],
     travelData: [],
@@ -34,9 +60,6 @@ const Homepage = () => {
       })
       .then((data) => {
         let falseData = data;
-        let trueData = data;
-        setArticlesData(data);
-
         falseData = falseData.data.filter((item) => item.is_featured === false);
         let sortedData = falseData.sort(
           (a, b) =>
@@ -45,11 +68,11 @@ const Homepage = () => {
         );
         setNonfeaturedData(sortedData);
 
-        let foodData = falseData;
-        let travelData = falseData;
-        let fashionData = falseData;
-        let filmData = falseData;
-        let businessData = falseData;
+        let foodData = sortedData;
+        let travelData = sortedData;
+        let fashionData = sortedData;
+        let filmData = sortedData;
+        let businessData = sortedData;
         foodData = foodData.filter((item) => item.categories.id === 1);
         travelData = travelData.filter((item) => item.categories.id === 2);
         fashionData = fashionData.filter((item) => item.categories.id === 3);
@@ -63,26 +86,126 @@ const Homepage = () => {
           businessData,
         });
 
+        let trueData = data;
         trueData = trueData.data.filter((item) => item.is_featured === true);
         setFeaturedData(trueData);
+
+        setArticlesData(sortedData);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+  const handleItemClick = (_, { name }) => {
+    setActiveItem(name);
+    switch (name) {
+      case "all":
+        setArticlesData(nonfeaturedData);
+        break;
+      case "fashion":
+        setArticlesData(categoryData.fashionData);
+        break;
+      case "film":
+        setArticlesData(categoryData.filmData);
+        break;
+      case "food":
+        setArticlesData(categoryData.foodData);
+        break;
+      case "travel":
+        setArticlesData(categoryData.travelData);
+        break;
+      case "business":
+        setArticlesData(categoryData.businessData);
+        break;
+      default:
+        return;
+    }
+  };
+
   return (
     <Grid>
       <StyledCard>
-        {categoryData.foodData &&
-          categoryData.foodData.length > 0 &&
-          categoryData.foodData.map((item, index) => {
+        <Grid.Column width={4} />
+        <Grid.Column width={4}>
+          <Button
+            basic
+            color="black"
+            name="all"
+            className={activeItem === "all" && "active"}
+            onClick={handleItemClick}
+          >
+            All Articles
+          </Button>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <Button
+            basic
+            color="black"
+            name="fashion"
+            className={activeItem === "fashion" && "active"}
+            onClick={handleItemClick}
+          >
+            Fashion & Beauty
+          </Button>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <Button
+            basic
+            color="black"
+            name="film"
+            className={activeItem === "film" && "active"}
+            onClick={handleItemClick}
+          >
+            Film
+          </Button>
+        </Grid.Column>
+        <Grid.Column width={4} />
+        <Grid.Column width={4} className="menu-border">
+          <Button
+            basic
+            color="black"
+            name="food"
+            className={activeItem === "food" && "active"}
+            onClick={handleItemClick}
+          >
+            Food & Drink
+          </Button>
+        </Grid.Column>
+        <Grid.Column width={4} className="menu-border">
+          <Button
+            basic
+            color="black"
+            name="travel"
+            className={activeItem === "travel" && "active"}
+            onClick={handleItemClick}
+          >
+            Travel
+          </Button>
+        </Grid.Column>
+        <Grid.Column width={4} className="menu-border">
+          <Button
+            basic
+            color="black"
+            name="business"
+            className={activeItem === "business" && "active"}
+            onClick={handleItemClick}
+          >
+            Business & Work
+          </Button>
+        </Grid.Column>
+        <Grid.Column width={16}>
+          <br />
+        </Grid.Column>
+        {articlesData &&
+          articlesData.length > 0 &&
+          articlesData.map((item, index) => {
             return (
               <Grid.Column key={index} width={8}>
                 <Image src={item.image} width="100%" height="300" />
                 <br />
                 <Label basic color="black">
-                  By {item.author}
+                  <span>By</span> {item.author}
                 </Label>
                 <h3>{item.title}</h3>
                 <br />
@@ -90,6 +213,11 @@ const Homepage = () => {
               </Grid.Column>
             );
           })}
+        <Grid.Column width={16}>
+          {articlesData && articlesData.length > 10 && (
+            <Button color="orange">More Articles</Button>
+          )}
+        </Grid.Column>
       </StyledCard>
     </Grid>
   );
